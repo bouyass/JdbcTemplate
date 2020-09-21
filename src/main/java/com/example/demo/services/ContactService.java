de1e5a39a9a2ea8c3084e47159644baf62bda396
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-09-22 01:27:10 +0200

    First commit

---
+++ src/main/java/com/example/demo/services/ContactService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+    public int addContact(){
+        return 0;
+    }
+
+    public int removeContact(){
+        return  0;
+    }
+
+    public int updateContact(){
+        return 0;
+    }
+
+}
